a7d1e0d166ff6f31dc2ea938ddc926a8a8b8d615
Adeelkhan5816 <adeelkhan5115@gmail.com>
2025-11-24 19:54:13 +0500

    RAMZAN LOGIC

---
+++ app.js
@@ -0,0 +1,25 @@
+// RAMZAN  LOGIC //
+
+
+
+let ramadanDate = new Date("2026-02-17T00:00:00");
+
+
+let currentDate = new Date();
+
+
+let time = ramadanDate.getTime() - currentDate.getTime();
+
+
+let totalDays = Math.floor(time / (1000 * 60 * 60 * 24));
+let totalWeeks = Math.floor(totalDays / 7);
+
+
+let totalMonths = Math.floor(totalDays / 30);
+
+
+console.log("Remaining time until Ramadan 2026:");
+console.log("Months: " + totalMonths);
+console.log("Weeks: " + totalWeeks);
+console.log("Days: " + totalDays);
+
